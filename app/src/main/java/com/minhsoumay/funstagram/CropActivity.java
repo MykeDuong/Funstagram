2d66908b4d99a215caaa39b39e97f07c029e6a2a
MykeDuong <hongminh4402@gmail.com>
2022-04-25 09:27:26 -0700

    Initialize the Post and CropImage Activities

---
+++ app/src/main/java/com/minhsoumay/funstagram/CropActivity.java
@@ -0,0 +1,23 @@
+package com.minhsoumay.funstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.canhub.cropper.CropImageActivity;
+
+public class CropActivity extends CropImageActivity {
+
+    com.canhub.cropper.CropImageView cropImageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+
+        cropImageView = findViewById(R.id.crop_image_view);
+
+        setCropImageView(cropImageView);
+    }
+
+}
--- app/src/main/java/com/minhsoumay/funstagram/PostActivity.java
+++ app/src/main/java/com/minhsoumay/funstagram/PostActivity.java
@@ -2,13 +2,61 @@ package com.minhsoumay.funstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.canhub.cropper.CropImage;
+import com.canhub.cropper.CropImageActivity;
+
+import javax.annotation.Nullable;
 
 public class PostActivity extends AppCompatActivity {
+
+    private Uri imageUri;
+
+    private ImageView close;
+    private ImageView imageAdded;
+    private TextView post;
+    EditText description;
+
+    Activity cropImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        close = findViewById(R.id.close);
+        imageAdded = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(PostActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        startActivity(new Intent(PostActivity.this, CropActivity.class));
+        //CropImageActivity.start(PostActivity.this);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            //CropImage.ActivityResult result = CropActivity.getActivityResult()
+            //imageUri =
+        }
+    }
+
 }
